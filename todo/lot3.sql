364cbdba16cbed3c917f116a1137d77b6413a743
MauiLBDev <maui.lb.dev@gmail.com>
2025-07-09 09:58:18 +0200

    finished lot3

--- todo/lot3.sql
+++ todo/lot3.sql
@@ -1,15 +1,54 @@
 -- ! Compter le nombre d'hôtel par staion
 
-    SELECT s.name, COUNT()
+    SELECT s.name, COUNT(h.id) AS hotel_count
+    FROM station s
+    JOIN hotel h
+    ON s.id = h.station_id
+    GROUP BY s.name;
 
 
 -- ! Compter le nombre de chambre par station
 
+    SELECT s.name, COUNT(r.id) AS room_count
+    FROM station s
+    JOIN hotel h
+    ON s.id = h.station_id
+    JOIN room r
+    ON h.id = r.hotel_id
+    GROUP BY s.name;
+
 
 -- ! Comtper le nombre de chambre par station ayant une capacité > 1
 
+    SELECT s.name, COUNT(r.id) AS room_count
+    FROM station s
+    JOIN hotel h
+    ON s.id = h.station_id
+    JOIN room r
+    ON h.id = r.hotel_id
+    WHERE r.capacity > 1
+    GROUP BY s.name;
 
 -- ! Afficher la liste des hôtels pour lesquels Mr Squire a effectué une réservation
 
+    SELECT c.last_name AS client, h.name AS hotel
+    FROM hotel h
+    JOIN room r
+    ON h.id = r.hotel_id
+    JOIN booking b
+    ON r.id = b.room_id
+    JOIN client c
+    ON c.id = b.client_id
+    WHERE c.last_name = 'Squire';
 
 -- ! Afficher la durée moyenne des réservations par station
+
+    SELECT s.name AS station_name, AVG(b.stay_end_date - b.stay_start_date) AS moyenne_resa
+    FROM station s
+    JOIN hotel h
+    ON s.id = h.station_id
+    JOIN room r
+    ON h.id = r.hotel_id
+    JOIN booking b
+    ON r.id = b.room_id
+    GROUP BY s.name;
--- todo/test_insert.sql
+++ todo/test_insert.sql
@@ -1,11 +1,13 @@
---! ATTENTION : l'initialisation des tables fait qu'il est nécessaire d'altérer la séquance 
+--! ATTENTION : l'initialisation des tables fait qu'il est nécessaire d'altérer la séquence 
 --! des "id" qui sont en auto-incrémentation
+
+-- ! ALTER SEQUENCE [séquence visé] RESTART [valeur souhaité]
 
 --? Insérer au moins 2 stations
 
-INSERT INT station (name, altitude) VALUES ('Le rocher Impératrice', 3000);
-INSERT INT station (name, altitude) VALUES ('Le cube', 2200);
-INSERT INT station (name, altitude) VALUES ('Pyramide', 2000);
+INSERT INTO station (name, altitude) VALUES ('Le rocher Impératrice', 3000);
+INSERT INTO station (name, altitude) VALUES ('Le cube', 2200);
+INSERT INTO station (name, altitude) VALUES ('Pyramide', 2000);
 
 --? Insérer au moins 2 hotel
 
@@ -25,4 +27,3 @@ INSERT INTO room (hotel_id, number, capacity, type) VALUES (10, 123, 3, 1);
 
 INSERT INTO client (first_name, last_name, address, city) VALUES ('Victor', 'McBernik', '11 rue des Océans', 'Le Relecq Kerhuon');
 INSERT INTO client (first_name, last_name, address, city) VALUES ('Lou', 'Affreu', '', 'Brest');
-INSERT INTO client (first_name, last_name, address, city) VALUES ('Abienenc***', 'Akim', 'Rue de la tiz', 'Paris');
